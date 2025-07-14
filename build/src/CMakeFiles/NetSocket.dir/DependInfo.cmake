fa84cdfed776eccc39beb47b252ce48ddeaa33bc
webserver <webserver@localhost.localdomain>
2025-07-14 05:08:05 -0700

    提交第一份源代码

---
+++ build/src/CMakeFiles/NetSocket.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/webserver/Project/NetSocket/src/base/AsyncLog.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/base/AsyncLog.cc.o"
+  "/home/webserver/Project/NetSocket/src/base/AsyncLog2.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/base/AsyncLog2.cc.o"
+  "/home/webserver/Project/NetSocket/src/base/CurrentThread.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/base/CurrentThread.cc.o"
+  "/home/webserver/Project/NetSocket/src/base/LogBuffer.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/base/LogBuffer.cc.o"
+  "/home/webserver/Project/NetSocket/src/base/Logger.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/base/Logger.cc.o"
+  "/home/webserver/Project/NetSocket/src/main.cpp" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/main.cpp.o"
+  "/home/webserver/Project/NetSocket/src/net/Acceptor.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Acceptor.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Buffer.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Buffer.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Channel.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Channel.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Connection.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Connection.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/DefaultPoller.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/DefaultPoller.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Epollpoller.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Epollpoller.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/EventLoop.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/EventLoop.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/EventLoopThread.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/EventLoopThread.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/EventLoopThreadPool.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/EventLoopThreadPool.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/InetAddress.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/InetAddress.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Poller.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Poller.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Socket.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Socket.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/TcpServer.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/TcpServer.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Thread.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Thread.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/Timestamp.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/Timestamp.cc.o"
+  "/home/webserver/Project/NetSocket/src/net/WorkerThreadPool.cc" "/home/webserver/Project/NetSocket/build/src/CMakeFiles/NetSocket.dir/net/WorkerThreadPool.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/base"
+  "../include/net"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
